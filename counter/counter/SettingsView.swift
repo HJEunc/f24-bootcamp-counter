cda1c7ee14cc2f73da09c2ffe97823f829528b24
Harrison Enyeart <enyeart@unc.edu>
2024-09-24 19:29:23 -0400

    finished

---
+++ counter/counter/SettingsView.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsView.swift
+//  counter
+//
+//  Created by Harrison Enyeart on 9/24/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @Binding var maxCount: Int
+    var body: some View {
+        Stepper("Max Count: \(maxCount)", value: $maxCount)
+            .font(.title)
+            .padding()
+            .background(Color.white, in: .rect(cornerRadius: 10))
+            .padding()
+    }
+}
+
+#Preview {
+    SettingsView(maxCount: .constant(5))
+}
